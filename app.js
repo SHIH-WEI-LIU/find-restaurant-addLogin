1bc4a122b4835bc86a80d7557661d300c4080ed6
SHIH-WEI-LIU <lsw850718@yahoo.com>
2022-08-22 17:46:47 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser') // 引用 body-parser(用來抓取res.
 const methodOverride = require('method-override') // 載入 method-override
 const routes = require('./routes') //引入路由器時，路徑設定為 /routes 就會自動去尋找目錄下叫做 index 的檔案
 const session = require('express-session')//載入session
+const usePassport = require('./config/passport')//載入config/passport
 require('./config/mongoose') //載入mongoose
 const port = process.env.PORT || 3000 //如果在 Heroku 環境則使用 process.env.PORT，否則為本地環境，使用 3000 
 
@@ -23,10 +24,12 @@ app.use(express.static('public'))
 app.use(methodOverride('_method'))
 //session
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: 'ThisISMySecret',
   resave: false,
   saveUninitialized: true
 }))
+//呼叫 Passport 函式並傳入 app
+usePassport(app)
 // 將 request 導入路由器
 app.use(routes)
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const restaurant = require('../../models/restaurant')
 const router = express.Router()
 const User = require('../../models/user')//引入建立好的 User model
 
@@ -7,9 +8,40 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
-//login
-router.get('/login', (req, res) => {
-  res.render('login')
+//signUp
+router.post('/signUp', (req, res) => {
+  // 取得註冊表單參數
+  const { name, email, password, confirmPassword } = req.body
+  // 檢查使用者是否已經註冊
+  User.findOne({ email })
+    .then(user => {
+      // 如果已經註冊：退回原本畫面
+      if (user) {
+        console.log('User already exists.')
+        res.render('login', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      } else {
+        // 如果還沒註冊：寫入資料庫
+        return User.create({
+          name,
+          email,
+          password
+        })
+          .then(() => res.redirect('/'))
+          .catch(err => console.log(err))
+      }
+    })
+    .catch(err => console.log(err))
+})
+
+//signIn
+// 加入 middleware，驗證 request 登入狀態
+router.post('/signIn', (req, res) => {
+  res.render('index')
 })
 
 module.exports = router
